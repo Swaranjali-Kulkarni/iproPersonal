4503c7649d537ba5222d47d32fd919045dcf191d
swaranjalik24@gmail.com <swaranjalik24@gmail.com>
2019-01-09 14:45:55 +0530

    generate quotation stepwise form

---
+++ application/views/footer_view1.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<link rel="stylesheet" href="<?php echo base_url();?>css/footer_css.css">
+</head>
+<body>
+<!--footer starts from here-->
+<br>
+<footer class="footer">
+<div class="container-fluid bottom_border" style="width: 100%">
+<div class="row">
+
+<div class=" col-sm-4 col-md  col-12 col" align="left">
+<h5 class="headin5_amrc col_white_amrc pt2">Our Services</h5>
+<!--headin5_amrc-->
+<ul class="footer_ul_amrc">
+<li><a href="3dprinting.php">3D Printing</a></li>
+<li><a href="3dscanning.php">3D Scanning</a></li>
+<li><a href="http://kalarikendramdelhi.com">Rapid Prototyping</a></li>
+<li><a href="http://kalarikendramdelhi.com">3D & 2D CAD Modelling</a></li>
+<li><a href="http://kalarikendramdelhi.com">Design for 3D Printing</a></li>
+<li><a href="http://kalarikendramdelhi.com">Reverse Engineering</a></li>
+<li><a href="http://kalarikendramdelhi.com">and many more..</a></li>
+</ul>
+<!--footer_ul_amrc ends here-->
+</div>
+
+
+<div class=" col-sm-4 col-md  col-12 col" align="left">
+<h5 class="headin5_amrc col_white_amrc pt2">About us</h5>
+<!--headin5_amrc-->
+<ul class="footer_ul_amrc">
+<li><a href="http://kalarikendramdelhi.com">Overview</a></li>
+<li><a href="http://kalarikendramdelhi.com">History</a></li>
+<li><a href="http://kalarikendramdelhi.com">Our Approach</a></li>
+<li><a href="http://kalarikendramdelhi.com">Our Team</a></li>
+<li><a href="http://kalarikendramdelhi.com">Company Ethics</a></li>
+<li><a href="http://kalarikendramdelhi.com">Authentic</a></li>
+<li><a href="http://kalarikendramdelhi.com">Accuracy</a></li>
+<li><a href="http://kalarikendramdelhi.com">Careers</a></li>
+</ul>
+<!--footer_ul_amrc ends here-->
+</div>
+
+<div class=" col-sm-4 col-md col-sm-4  col-12 col" align="left">
+<h5 class="headin5_amrc col_white_amrc pt2">Contact us</h5>
+<!--headin5_amrc-->
+<ul class="footer_ul_amrc">
+<li><a href="http://kalarikendramdelhi.com">Support/FAQs</a></li>
+<li><a href="http://kalarikendramdelhi.com">Enquiries</a></li>
+<li><a href="http://kalarikendramdelhi.com">Contact</a></li>
+<li><a href="http://kalarikendramdelhi.com">Feedback Form</a></li>
+<li><a href="http://kalarikendramdelhi.com">Get Quotation</a></li>
+</ul>
+<p><i class="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35 </p>
+<p><i class="fa fa-phone"></i>  +91-+91 9821413663</p>
+<p><i class="fa fa fa-envelope"></i>hello@ipro3d.io</p>
+</div>
+
+</div>
+</div>
+
+
+<div class="container">
+<ul class="foote_bottom_ul_amrc">
+<li><a href="http://kalarikendramdelhi.com">Home</a></li>
+<li><a href="http://kalarikendramdelhi.com">About Us</a></li>
+<li><a href="http://kalarikendramdelhi.com">Services</a></li>
+<li><a href="http://kalarikendramdelhi.com">Portfolio</a></li>
+<li><a href="http://kalarikendramdelhi.com">Marketplace</a></li>
+<li><a href="http://kalarikendramdelhi.com">Mutation</a></li>
+<li><a href="http://kalarikendramdelhi.com">Contact</a></li>
+<li><a href="http://kalarikendramdelhi.com">Login</a></li>
+</ul>
+<!--foote_bottom_ul_amrc ends here-->
+
+<div class="w3-xxlarge">
+                <a href="https://www.facebook.com/ipro3d.tech"> <i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+                <a href="https://www.instagram.com/ipro3d"> <i class="fa fa-instagram w3-hover-opacity"></i></a>
+                <a href=" https://twitter.com/ipro3d"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+                <a href="https://www.linkedin.com/company/ipro3d-technologies-llp"> <i class="fa fa-linkedin w3-hover-opacity"></i></a>
+                <a href="https://api.whatsapp.com/send?phone=919821413663" class="float" target="_blank">
+                    <i class="fa fa-whatsapp my-float"></i></a>
+            </div><!--social_footer_ul ends here-->
+</div>
+
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
