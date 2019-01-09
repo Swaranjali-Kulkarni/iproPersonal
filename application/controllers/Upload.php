6e8d6a41f177c6b2780c9b06f450aef239ecc588
deepika-sodhi <deepikasodhi5@gmail.com>
2019-01-09 12:52:04 -0500

    file commit

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -37,22 +37,7 @@ class Upload extends CI_Controller {
 
 
     
-        public function myupload(){
-            $config['upload_path']          = 'uploads/images/package';
-            $config['allowed_types']        = 'gif|jpg|png';
-            $config['encrypt_name']         = true;
-            $this->load->library('upload', $config);
-            if ( ! $this->upload->do_upload('userfile')) {
-            $error = array('error' => $this->upload->display_errors());
-            $this->load->view('upload_form', $error);
-        }
-
-        else {
-            $data = array('upload_data' => $this->upload->data());
-            $this->load->view('upload_success', $data);
-        }
-        }
-
+       
 
 
     public function database_fun()
